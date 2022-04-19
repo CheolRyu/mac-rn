3e6159c610dd898140d035199343ae8d3e88f1ac
Charles Ryu <cryu@22CRyu-MBP2XDCJ.local>
2022-04-19 10:21:05 -0700

    created more automations in Context creation, refactored handling data, updating data, CRUD

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,10 +1,19 @@
 import React, { useReducer } from 'react';
 
-export default (reducer, action, inintialState) => {
+export default (reducer, actions, inintialState) => {
   const Context = React.createContext();
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, inintialState);
-    return <Context.Provider value={{ state }}>{children}</Context.Provider>;
+    //action === {addBlogPost: (dispatch) => {return () => {}}}
+    const boundActions = {};
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
+    return (
+      <Context.Provider value={{ state, ...boundActions }}>
+        {children}
+      </Context.Provider>
+    );
   };
   return { Context, Provider };
 };
