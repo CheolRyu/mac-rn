f0a47f8abf8281c3a0a411fff17bb8b7c4138369
Charles Ryu <cryu@22CRyu-MBP2XDCJ.local>
2022-04-19 10:38:11 -0700

    applyed simple CSS on post lists

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,24 +1,45 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogConext';
+import { Feather } from '@expo/vector-icons';
 
 const Indexscreen = () => {
   const { state, addBlogPost } = useContext(Context);
   return (
     <View>
-      <Text>Hello from index Screen</Text>
       <Button title='Add Post' onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather name='trash' style={styles.icon} />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    paddingHorizontal: 10,
+    borderColor: 'grey',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+    color: 'red',
+  },
+});
 
 export default Indexscreen;
