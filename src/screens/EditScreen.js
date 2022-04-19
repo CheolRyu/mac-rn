7bd062845747fc6330e71ce935e42b1e0ceb6bc9
Charles Ryu <cryu@22CRyu-MBP2XDCJ.local>
2022-04-19 12:23:46 -0700

    added a method to take user directly to Index page upon adding a post, also added a edit screen, and implemented a headerRight navigation to edit the post

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const EditScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Edit</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogConext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -12,10 +13,20 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
 
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <EvilIcons name='pencil' size={35} />
+      </TouchableOpacity>
+    ),
+  };
+};
 const styles = StyleSheet.create({});
 
 export default ShowScreen;
