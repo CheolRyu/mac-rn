a230defe5b0259377f951cdb1f83496af57c36b6
Charles Ryu <cryu@22CRyu-MBP2XDCJ.local>
2022-04-19 09:50:45 -0700

    reducer on blog post

--- src/context/BlogConext.js
+++ src/context/BlogConext.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext(); // a custom component that will accept data as children so that we can pass it to many components.
 
-export const BlogProvider = ({ children }) => {
-  {
-    children;
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
   }
-  return <BlogContext.Provider />;
 };
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogConext';
 
 const Indexscreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Hello from index Screen</Text>
+      <Button title='Add Post' onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
